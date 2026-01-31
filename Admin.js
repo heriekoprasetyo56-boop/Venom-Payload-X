add4c647fb7541fbf3f4bd4f8eb5df4d07d9aeed
HeriTezzRorw <235550888+heriekoprasetyo56-boop@users.noreply.github.com>
2026-02-01 00:28:31 +0700

    Delete Admin.js

--- Admin.js
+++
@@ -1,7 +0,0 @@
-const adminList = require('../admin.json'); // Import dari luar folder admin
-
-const isAdmin = (userId) => {
-  return adminList.includes(userId);
-};
-
-module.exports = { isAdmin };
